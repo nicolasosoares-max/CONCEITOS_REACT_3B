2f290c6970c65cf34c2fbd8b40654266ae1b685d
Aluno SENAI <Aluno@SN-396878.edu.fiesc.com.br>
2026-04-30 11:41:23 -0300

    login e cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './paginas/login'
+import Cadastro from './paginas/cadastro'
+
+
 
 function App() {
+const [tela, setTela] = useState('login')
+
+
 const renderizarTela = () => {
-  // navegação
+ if(tela === 'login') {
+  return <Login/>
+ } else if (tela === 'cadastro') {
+  return <Cadastro/>
+ } 
 }
+
+  const trocarTela = (pagina) => {
+    setTela(pagina)
+  }
+
 
   return (
     <>
-      <section id="center">
-        
-      </section>
+      <h1>picachu</h1>
+      <button onClick={ () => trocarTela('login')}>login</button>
+      <button onClick={ () => trocarTela('cadastro')}>cadastro</button>
+      {renderizarTela()}
     </>
   )
 }
